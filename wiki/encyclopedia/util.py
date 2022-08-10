d9242e7663c80267feb4891dd5a29d123c78eceb
Pavlo <pavloprocenko64@gmail.com>
2022-08-10 14:51:07 +0300

    4

--- wiki/encyclopedia/util.py
+++ wiki/encyclopedia/util.py
@@ -2,6 +2,7 @@ import re
 
 from django.core.files.base import ContentFile
 from django.core.files.storage import default_storage
+from django.core.files import File
 
 
 def list_entries():
@@ -14,17 +15,25 @@ def list_entries():
 
 
 def save_entry(title, content):
+    
+    filename = f"entries/{title}.md"
+    with open(filename, "w", encoding="utf-8") as f:
+        file = File(f)
+        file.write(content)
+    
+
     """
     Saves an encyclopedia entry, given its title and Markdown
     content. If an existing entry with the same title already exists,
     it is replaced.
     """
+    """
     filename = f"entries/{title}.md"
     if default_storage.exists(filename):
         default_storage.delete(filename)
     default_storage.save(filename, ContentFile(content))
-
-
+    """
+   
 def get_entry(title):
     """
     Retrieves an encyclopedia entry by its title. If no such
@@ -32,6 +41,6 @@ def get_entry(title):
     """
     try:
         f = default_storage.open(f"entries/{title}.md")
-        return f.read().decode("Windows-1251")#"utf-8"
+        return f.read().decode("utf-8")
     except FileNotFoundError:
         return None
